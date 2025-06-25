67f044102c15edfba53086d00ea72cf967c0d2d2
Pavan Kumar Pedamallu <pavank0066@gmail.com>
2025-06-25 09:44:05 +0530

    Temperature Converter files added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'temperature-converter';
+
+  celcius: any = '';
+  fahrenheit: any = '';
+  resC: any = '';
+  resF: any = '';
+
+  celciusToFahrenheit(){
+    this.resF = (this.celcius * 9/5) + 32; 
+  }
+
+  fahrenheitToCelcius(){
+    this.resC = (this.fahrenheit - 32) * 5/9;
+  }
+
 }
